48b7358e0aca1a224e6a3537a4ff21e99a77d806
Elitsa Venchovs <elica_vladi@abv.bg>
2021-07-20 11:51:34 +0300

    html
    Added template of html page

--- Scan-y/app.py
+++ Scan-y/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('main.html')
+    # return render_template('main.html', cities=cities) #За подаване на данни към template
 
 
 if __name__ == '__main__':
