4df8ffdbd8e8e235229b1cd2759f3f43143151ee
ElitsaVenchova <elica_vladi@abg.bg>
2021-07-24 10:21:35 +0300

    Commit before code fixes and run

--- demos/WebStreamPyCamera/webstreaming.py
+++ demos/WebStreamPyCamera/webstreaming.py
@@ -1,5 +1,5 @@
 # import the necessary packages
-from WebStreamPyCamera.motion_detection import SingleMotionDetector
+from pyimagesearch.motion_detection import SingleMotionDetector
 from imutils.video import VideoStream
 from flask import Response
 from flask import Flask
@@ -43,3 +43,88 @@ def detect_motion(frameCount):
 
     # loop over frames from the video stream
     while True:
+        # read  the next frame from the video stream, resize it,
+        # convert the frame to grayscale, and blur it
+        frame = vs.read()
+        frame = imutils.resize(frame, width = 400)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+        # grab the current timestamp and draw it on the frame
+        timestamp = datetime.datetime.now()
+        cv2.putText(frame, timestamp.strftime(
+            "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+                    cv2.FONT_HERSHEY_SIMPLE, 0.35, (0, 0, 255), 1)
+
+        # if the total number of frames has reached a sufficient
+        # number to construct a reasonable background model, then
+        # continue to process the frame
+        if total > frameCount:
+            # total motion in the image
+            motion = md.datect(gray)
+
+            # check to see if motion was found in the frame
+            if motion is not None:
+                # unpack the tuple and draw the box surrounding the
+                # "motion area" on the output frame
+                (thresh, (minX, minY, maxX, maxY)) = motion
+                cv.rectangle(frame, (minX, minY), (maxX, maxY),
+                (0, 0, 255), 2)
+
+        # update the background model and increment the total number
+        # of frames read thus far
+        md.update(gray)
+        total += 1
+
+        # acquire the lock, set output frame, and release the
+        # lock
+        with lock:
+            outputFrame = frame.copy()
+
+def generate():
+    # grab gobal refereences to the output frame and lock variables
+    global outputFrame, lock
+
+    # loop over frames from the output stream
+    while True:
+        # wait until the lock is aquired
+        with lock:
+            # check if the output frame is avaible, otherwise skip
+            # the iteration of the loop
+            if outputFrame is None:
+                continue
+
+        # yield the output frame in the byte format
+        yield(b'--format\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+              bytearray(encodeImage) + b'\r\n')
+
+@app.route("/video_feed")
+def video_feed():
+    # return the responce generated along with specific media
+    # type (mime type)
+    return Response(generate(),
+                    mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+if __name__ == '__main__':
+    # construct the argument parser and parse command line arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--ip", type=str, required=True,
+                    help="ip addresss of the device")
+    ap.add_argument("-o", "--port", type=int, required=True,
+                    help="ephemeral port number of the server (1024 to 65535)")
+    app.add_argument("-f", "--frame-count", type=int, default=32,
+                     help="# of frames used to construct the background model")
+    args = vars(ap.parse_args())
+
+    # start a thread that will perform motion detection
+    t = threading.Thread(target=detect_motion, args=(
+        args["frame_count"],))
+    t.deamon = True
+    t.start()
+
+    # start the flask app
+    app.run(host=args["ip"], port=args["port"], debug=True,
+            threaded=True, use_reloader=False)
+
+    # release the video stream pointer
+    vs.stop()
