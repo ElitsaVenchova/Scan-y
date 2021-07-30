89af9da95814f469604a2546757fc1008c636da5
ElitsaVenchova <elica_vladi@abg.bg>
2021-07-30 23:57:23 +0300

    Added demo with video play

--- demos/turn_off_and_on_hdmi_demo.py
+++ demos/turn_off_and_on_hdmi_demo.py
@@ -1,8 +1,32 @@
-import time
 import subprocess
 
-while True:
-	subprocess.call('vcgencmd display_power 1', shell=True)
-	time.sleep(5)
-	subprocess.call('vcgencmd display_power 0', shell=True)
-	time.sleep(5)
+# importing vlc module
+import vlc
+# importing time module
+import time
+
+try:
+    # turn off hdmi
+    subprocess.call('vcgencmd display_power 0', shell=True)
+    time.sleep(5)
+    # turn on hdmi
+    subprocess.call('vcgencmd display_power 1', shell=True)
+
+    # creating vlc media player object
+    media_player = vlc.MediaPlayer()
+    # toggle full screen
+    media_player.toggle_fullscreen()
+    # media object
+    media = vlc.Media("pexels-cup-of-couple-8014403.mp4")
+    # setting start and stop time
+    media.add_option("start-time=0.0")
+    media.add_option("end-time=13.0")
+    # seting media to the media player
+    media_player.set_media(media)
+    # start playing media
+    media_player.play()
+    time.sleep(15)
+    media_player.stop()
+finally:
+    # turn on hdmi
+    subprocess.call('vcgencmd display_power 1', shell=True)
